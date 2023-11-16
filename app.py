70ac2f6a569bfe327258672c6e76cac18ba326c2
ashmi721 <ashmitak721@gmail.com>
2023-11-16 16:28:58 +0545

    successfully complete the login logic

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class Flipkart:
         
     def menu(self):
         
-      user_input=  input("""
+      user_input = input("""
         1 . Enter 1 to register
         2 . Enter 2 to login
         3 . Anything else to leave
@@ -22,18 +22,32 @@ class Flipkart:
       else:
           sys.exit(1000)          
       
+      
+      
     def register(self):
-        name = input("Enter the name")
-        email = input("Enter the email")
-        password = input("Enter the password")
+        name = input("Enter the name:")
+        email = input("Enter the email:")
+        password = input("Enter the password:")
         
     
         response = self.db.register(name,email,password)
         
-        if response:
+        if response :
             print("Registration successful")
         else:
             print("Registration failed")   
             
-        self.menu()      
+        self.menu()   
+        
+    def login(self):
+        email = input("Enter email:")
+        password = input("Enter password:")
+        
+        data = self.db.search(email,password)
+        # print(data)
+        if len(data) == 0:
+            print("Incorrect email or password")
+            self.login()
+        else:
+             print("Hello",data[0][1])          
 obj = Flipkart()                           
--- dbhelper.py
+++ dbhelper.py
@@ -1,25 +1,35 @@
 import mysql.connector
-
+import sys
 class DBhelper:
     def __init__(self):
         try:
-            self.conn= mysql.connector.connect(host="localhost",username="root",password = "",
+            self.conn= mysql.connector.connect(host="localhost",user="root",password = "",
                                         database="flip")
             self.mycursor = self.conn.cursor()
-        except:
-            print("Connected to Database")
-            
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+            sys.exit(1)
         else:
-            print("Some error occured")    
-            
-    def register(self,name,email,password):
+            print("Connected to Database")  
+                
+    def register(self, name, email, password):
         try:
             self.mycursor.execute("""
-            InSERT INTO `users` (`id`,`name`,`email`,`password`)VALUES(NULL,'{}','{}','{}')                      
-                                """.format(name,email,password))
-            self.conn.commit() 
-        except:
+            INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, %s, %s, %s)
+            """, (name, email, password))
+            self.conn.commit()
+        except Exception as e:
+            print(f"Error during registration: {e}")
             return -1
         else:
-            return 1    
-             
+            return 1
+   
+    
+    def search(self,email,password):
+        self.mycursor.execute(""" SELECT * FROM `users` WHERE email LIKE '{}' AND password LIKE '{}' 
+                """.format(email,password))
+     
+        data = self.mycursor.fetchall()
+        
+        return data
+                 
